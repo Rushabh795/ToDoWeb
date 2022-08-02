0e994b4660d1f33f9122726939557da67e4fc08f
Rushabh Shah <rushabh795@gmail.com>
2022-08-02 14:47:59 -0400

    Add insert method to controller

--- ToDo/Controllers/HomeController.cs
+++ ToDo/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.Sqlite;
 using Microsoft.Extensions.Logging;
 using ToDo.Models;
 
@@ -22,6 +23,27 @@ namespace ToDo.Controllers
         {
             return View();
         }
+        public void Insert(ToDoItem todoItem)
+        {
+            using (SqliteConnection con =
+                       new SqliteConnection("Data Source=db.sqlite"))
+            {
+                using (var tableCmd = con.CreateCommand())
+                {
+                    con.Open();
+                    tableCmd.CommandText = $"INSERT INTO todo (name) VALUES ('{todoItem.Name}')";
+                    try
+                    {
+                        tableCmd.ExecuteNonQuery();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+        }
     }
+    
 }
 
